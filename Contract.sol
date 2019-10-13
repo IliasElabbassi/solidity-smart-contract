576e17009ad2465537afdba2899781dcf46384c0
Ilias El abbassi <38820888+IliasElabbassi@users.noreply.github.com>
2019-10-13 21:21:02 +0200

    Solidity Smart Contract template

---
+++ Contract.sol
@@ -0,0 +1,33 @@
+pragma solidity #version#;
+
+// Imports
+import "#path#";
+
+// Contract Class
+contract #ContractName# {
+	// Constant declaration
+	// Variable declaration
+
+	//Constructor
+	constructor #ContractName# () {
+		// instruction
+	}
+
+	// Events
+	event #eventName# () {}
+
+	/**
+		Functions
+		Within a function (standard -> view -> pure)
+	*/
+
+	// fallback functions
+
+	// External functions
+
+	// Public functions
+
+	// Internal functions
+
+	// Private functions
+}
